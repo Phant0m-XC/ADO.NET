a0dcd3ca2588c0f268b10edd21421a592faff0c7
Ph@nt0m-X <phantom-x@inbox.ru>
2017-05-11 22:23:39 +0300

    Lab2 done

--- ADO.NET/Lab2_1/Program.cs
+++ ADO.NET/Lab2_1/Program.cs
@@ -10,6 +10,21 @@ namespace Lab2_1
     {
         static void Main(string[] args)
         {
+            var item1 = new { Name = "item1", justValue = 5 };
+            var item2 = new { value1 = 15, valueBool = true };
+            Console.WriteLine(item1.Equals(item2));
+            Console.WriteLine("HashCode Item1: " + item1.GetHashCode());
+            Console.WriteLine("HashCode Item2: " + item2.GetHashCode());
+
+            var item3 = new { Name = "item1", justValue = 5 };
+            Console.WriteLine(item3.Equals(item1));
+            Console.WriteLine("HashCode Item1: " + item1.GetHashCode());
+            Console.WriteLine("HashCode Item3: " + item3.GetHashCode());
+            /*
+            Вывод: компилятор, при создании двух анонимных типов, инициализированных одинаковыми
+            значениями, применяет оптимизацию и по существу у нас под именами item1 и item2 находиться
+            один и тот же объект
+            */
         }
     }
 }
--- ADO.NET/Lab2_2/Program.cs
+++ ADO.NET/Lab2_2/Program.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Lab2_2
 {
-    
-
     class Program
     {
         static void Main(string[] args)
         {
+            string email;
+            do
+            {
+                Console.Write("Введите e-mail: ");
+                email = Console.ReadLine();
+                if (email.CheckEmail(email))
+                    Console.WriteLine("Верный");
+                else
+                    Console.WriteLine("Не верный");
+            } while (email != "");
+            Console.WriteLine("Программа завершена!");
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static bool CheckEmail(this string str, string email)
+        {
+            Regex reg = new Regex(@"(.+)(@)(.+)\.(.+)$");
+            Match match = reg.Match(email);
+            return match.Success;
         }
     }
 }
