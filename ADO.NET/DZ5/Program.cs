8bcf427ccb874527a82126167c745f0ebe6a34a2
Ph@nt0m-X <phantom-x@inbox.ru>
2017-05-13 18:28:23 +0300

    DZ5 done Lab3 correct

--- ADO.NET/DZ5/Program.cs
+++ ADO.NET/DZ5/Program.cs
@@ -12,7 +12,7 @@ namespace DZ5
         {
             List<Department> departments = new List<Department>()
             {
-                new Department() {Id =1, Country = "Ukraine", City = "Donetsk" },
+                new Department() {Id =1, Country = "Ukraine", City =  "Donetsk"},
                 new Department() {Id =2, Country = "Ukraine", City = "Kyiv" },
                 new Department() {Id =3, Country = "France", City = "Paris" },
                 new Department() {Id =4, Country = "Russia", City = "Moscow" },
@@ -28,6 +28,39 @@ namespace DZ5
                 new Employee() {Id = 6, FirstName = "Ivan", LastName = "Kalyta", Age = 22, DepId = 2 },
                 new Employee() {Id = 7, FirstName = "Nikita", LastName = "Kotov", Age = 27, DepId = 4 },
             };
+
+            //1
+            Console.WriteLine("Имена сотрудинков из Украины, но не из Донецка:");
+            var result1 = from c in departments
+                          where c.Country == "Ukraine" && c.City != "Donetsk"
+                          select c;
+            foreach (var item1 in result1)
+                foreach (var item2 in employees)
+                    if (item1.Id == item2.DepId)
+                        Console.WriteLine(item2.FirstName + " " + item2.LastName);
+
+            //2
+            Console.WriteLine("\nСтраны: ");
+            var result2 = departments.GroupBy(c => c.Country).Select(g => new { g.Key });
+            foreach (var item in result2)
+                Console.WriteLine(item.Key);
+
+            //3
+            Console.WriteLine("\n3 сотрудника старше 25 лет: ");
+            var result3 = (from c in employees
+                          where c.Age > 25
+                          select c).Take(3);
+            foreach(var item in result3)
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+
+            //4
+            Console.WriteLine("\nСотрудника старше 23 лет из Киева: ");
+            var result4 = from c in employees
+                          join t in departments on c.DepId equals t.Id
+                          where t.City == "Kyiv"
+                          select c;
+            foreach (var item in result4)
+                Console.WriteLine(item.FirstName + " " + item.LastName + " " + item.Age);
         }
     }
     class Employee
--- ADO.NET/Lab3/Program.cs
+++ ADO.NET/Lab3/Program.cs
@@ -23,30 +23,24 @@ namespace Lab3
             var result2 = context.Products.GroupBy(c => c.Category, c => c.Title).Select(
                 g => new { Category = g.Key, Count = g.Distinct().Count() });
             foreach (var item in result2)
-            {
                 Console.WriteLine(item);
-            }
 
             //3
             Console.WriteLine("\nТовары на 'S' с длинной строки 5 символов: ");
             var result3 = from c in context.Products
-                          where c.Title.StartsWith("S")
+                          where c.Title.StartsWith("S") && c.Title.Length == 5
                           select c;
             foreach (var item in result3)
-            {
-                if (item.Title.Length == 5)
-                    Console.WriteLine(item.Title);
-            }
+                Console.WriteLine(item.Title);
 
             //4
             Console.WriteLine("\nТовары в алфавитном порядке дешевле 400 грн: ");
             var result4 = from c in context.Products
-                          where c.Price < 400 orderby c.Title ascending
+                          where c.Price < 400
+                          orderby c.Title ascending
                           select c;
             foreach (var item in result4)
-            {
                 Console.WriteLine(item.Title);
-            }
 
             //5
             Console.WriteLine("\nКатегория с товаром с максимальной ценой: ");
